976df08bbab7177f07fd61c7505d705cafafee91
brandon-tl <brandon.timeleaper@gmail.com>
2023-10-27 02:48:19 +0200

    DELETE 'theme-button.js'

--- Duolingo notes/scripts/main.js
+++ Duolingo notes/scripts/main.js
@@ -1,2 +1,1 @@
 import './toggle-theme.js';
-import './theme-button.js';
--- Duolingo notes/scripts/theme-button.js
+++
@@ -1,9 +0,0 @@
-import { storageKey } from "./toggle-theme.js";
-
-$('#theme-toggle').click(() => {
-    // if (localStorage.getItem(storageKey) && storageKey === 'dark') {
-    //     localStorage.setItem(storageKey, 'light');
-    // } else if (localStorage.getItem(storageKey) && storageKey === 'light') {
-    //     localStorage.setItem(storageKey, 'dark');
-    // }
-});
