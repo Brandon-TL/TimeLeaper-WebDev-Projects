ba278c0c9120fb841ac1f0797ea4de1aad5eb619
brandon-tl <brandon.timeleaper@gmail.com>
2023-10-27 04:31:52 +0200

    UPDATE 'GSAP scroll animation'

--- Timeline/README.md
+++ Timeline/README.md
@@ -1,1 +1,13 @@
-# Horizontal-Scroll-and-Backgorund-animation
+# Horiozntal Scroll and Background Animation made with Gsap
+
+Find this project also on CodePen:
+    https://codepen.io/brandon-tl/pen/MWZLyzY
+
+INFO CREDITS:
+    2017: https://mashable.com/article/top-10-tech-stories-2017   
+    2018: https://www.history.com/topics/21st-century/2018-events
+    2019: https://www.history.com/topics/21st-century/2019-events
+    2020: https://www.history.com/topics/21st-century/2020-events
+    2021: https://www.history.com/topics/21st-century/2021-events
+    2022: https://www.cfr.org/blog/ten-most-significant-world-events-2022
+    2023: https://syltec.es/blog/2023/01/16/inteligencia-artificial-en-2023/
--- Timeline/script.js
+++ Timeline/script.js
@@ -1,68 +1,69 @@
+/* -- EDIT scrollVelocity TO SET HORIZONTAL SCROLL VELOCITY -- */
+
+const scrollVelocity = 3000;
+
 // Gsap horizontal scroll animation
 let sections = gsap.utils.toArray(".container");
 
 let scrollTween = gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none", // <-- IMPORTANT!
-  scrollTrigger: {
-    trigger: ".wrapper",
-    pin: true,
-    scrub: 3,
-    end: "+=8000"
-  }
+	xPercent: -100 * (sections.length + 0.25),
+	ease: "none", // <-- IMPORTANT!
+	scrollTrigger: {
+		trigger: ".wrapper",
+		pin: true,
+		scrub: 3,
+		end: "+=" + scrollVelocity
+	}
 });
 
-// Make marker visible to easy edit animation on scroll triggers position
+/* -- MAKE MARKERS VISIBLE TO EASILY EDIT ANIMATION SCROLL TRIGGERS POSITION -- */
+
 // ScrollTrigger.defaults({
 //     markers: {
+//         display: "fixed",
 //         startColor: "white",
 //         endColor: "white"
 //     }
 // });
 
-// red section
-document.querySelectorAll('.component').forEach(component => {
-  gsap.to(component, {
-    y: -80,
-    filter: " blur(0) brightness(1) opacity(1)",
-    duration: .3,
-    ease: "none",
-    scrollTrigger: {
-      trigger: component,
-      containerAnimation: scrollTween,
-      start: "-50 center",
-      end: "100% center",
-      toggleActions: "play reverse restart reverse",
-      onEnter: () => {
-        changeBg(component.querySelector('img').src);
-        flashYear(component.querySelector('.img-year').textContent);
-        component.classList.add('active');
-      },
-      onLeave: () => {
-        component.classList.remove('active');
-      },
-      onEnterBack: () => {
-        changeBg(component.querySelector('img').src);
-        flashYear(component.querySelector('.img-year').textContent);
-        component.classList.add('active');
-      },
-      onLeaveBack: () => {
-        component.classList.remove('active');
-      },
-    }
-  });
+/* -- CREATE ANIMATION -- */
+
+document.querySelectorAll(".component").forEach((component) => {
+	gsap.to(component, {
+		y: -80,
+		filter: " blur(0) brightness(1) opacity(1)",
+		duration: 0.3,
+		ease: "none",
+		scrollTrigger: {
+			trigger: component,
+			containerAnimation: scrollTween,
+			start: "-50 center",
+			end: "100% center",
+			toggleActions: "play reverse restart reverse",
+			onEnter: () => {
+				changeBg(component.querySelector("img").src);
+				component.classList.add("active");
+			},
+			onLeave: () => {
+				component.classList.remove("active");
+			},
+			onEnterBack: () => {
+				changeBg(component.querySelector("img").src);
+				component.classList.add("active");
+			},
+			onLeaveBack: () => {
+				component.classList.remove("active");
+			}
+		}
+	});
 });
 
-
-// Change background image
-function changeBg (src) {
-    document.querySelector('.duppled-bg').style.backgroundImage = "url("+src+")";
+/**
+ * Set event image to background scroll makes it visible on page
+ * @param src source link to the image to be displayed
+ * @returns void
+ */
+function changeBg(src) {
+	document.querySelector(".duppled-bg").style.backgroundImage =
+		"url(" + src + ")";
 }
-
-// Flash component's year at the background when animates in (not finished)
-function flashYear(year) {
-	const container = document.querySelector(".flash-container");
-
-	container.innerHTML = "";
-	container.insertAdjacentHTML("beforeend", `<div class="flash">${year}</div>`);
-} 
